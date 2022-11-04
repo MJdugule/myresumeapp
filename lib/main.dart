22ad7b39346bee57ab621d35d6902d2e831f0e55
MJdugule <pianyrain@gmail.com>
2022-11-04 22:18:43 +0000

    for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:my_portfolio/Section/landing.dart';
 import 'package:my_portfolio/home.dart';
 
 Future<void> main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-   
-  apiKey: "AIzaSyAC7CsEYB_yC_4P93FA0cOuY1hNAJKjgag",
-  authDomain: "chat-1d2dc.firebaseapp.com",
-  projectId: "chat-1d2dc",
-  storageBucket: "chat-1d2dc.appspot.com",
-  messagingSenderId: "834023371366",
-  appId: "1:834023371366:web:ac6b188323db80222cb3a7",
-  measurementId: "G-LGK129E8JH"
-  )
-
-  );
+  
   runApp(const MyApp());
 }
 
